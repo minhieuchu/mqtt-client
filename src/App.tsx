affadef660b167c3940f47d9a8ce5bf918cdbdc8
Hieu Chu <minhieuchu@gmail.com>
2023-07-22 20:48:31 +0700

    Subscribe to Device topic & Temperature topic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,59 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import mqtt from "precompiled-mqtt";
 import NavBar from "./components/NavBar";
 import Device from "./pages/Device";
 import DashBoard from "./pages/DashBoard";
 import ErrorPage from "./pages/ErrorPage";
 
+const DEVICE_TOPIC = "/device/6f9ea7c7-6297-4283-b72d-7d673d3473fd";
+const TEMPERATURE_TOPIC = DEVICE_TOPIC + "/Temperature";
+
 export default function App() {
+  const [mqttClient, setMqttClient] = useState<mqtt.MqttClient | null>(null);
+
+  useEffect(() => {
+    const host = "broker.emqx.io";
+    const port = 8083;
+    const url = `ws://${host}:${port}/mqtt`;
+    const options = {
+      clean: true,
+      connectTimeout: 5 * 1000,
+      reconnectPeriod: 1000,
+    };
+
+    const client = mqtt.connect(url, options);
+    setMqttClient(client);
+  }, []);
+
+  useEffect(() => {
+    if (mqttClient) {
+      mqttClient.on("connect", () => {
+        console.log("Mqtt connected");
+      });
+
+      mqttClient.on("message", (topic, message) => {
+        if (topic === DEVICE_TOPIC) {
+          // console.log(JSON.parse(message.toString()))
+        } else if (topic === TEMPERATURE_TOPIC) {
+          // console.log(JSON.parse(message.toString()))
+        }
+      });
+
+      mqttClient.subscribe(DEVICE_TOPIC, { qos: 0 }, (err) => {
+        if (err) {
+          console.log("Subscribe Device topic error ", err);
+        }
+      });
+
+      mqttClient.subscribe(TEMPERATURE_TOPIC, { qos: 0 }, (err) => {
+        if (err) {
+          console.log("Subscribe Temperature topic error ", err);
+        }
+      });
+    }
+  }, [mqttClient]);
+
   return (
     <BrowserRouter>
       <NavBar />
--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import mqtt from "precompiled-mqtt";
 import App from "./App.tsx";
 import "./index.css";
-
-let mqttClient: mqtt.MqttClient;
-const host = "broker.emqx.io";
-const port = 8083;
-
-function setupMqttClient(host: string, port: number) {
-  const url = `ws://${host}:${port}/mqtt`;
-  const options = {
-    clean: true,
-    connectTimeout: 5 * 1000,
-    reconnectPeriod: 1000,
-  };
-
-  mqttClient = mqtt.connect(url, options);
-
-  mqttClient.on("connect", () => {
-    console.log("Mqtt connected");
-  });
-
-  mqttClient.on("message", (topic, message) => {
-    const payload = { topic, message: message.toString() };
-    console.log(payload);
-  });
-
-  mqttClient.subscribe("device/random-device-id", { qos: 0 }, (err) => {
-    if (err) {
-      console.log("Subscribe error ", err);
-    }
-  });
-}
-
-setupMqttClient(host, port);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
